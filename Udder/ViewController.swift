3f9a4411726374d03b64250ec5fbb7c81c9ea5a7
Angadjot singh <angadjotsingh@angadjots-macbook-air.local>
2018-12-06 16:27:17 +0530

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Angadjot singh on 06/12/18.
+//  Copyright © 2018 Angadjot singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
